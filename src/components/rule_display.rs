168de992b924b23d5d822cf137cf2205334fe5a3
mjmena <martinjxm@gmail.com>
2025-04-04 14:38:46 -0400

    style: stabalize layout and make rule bigger
    feat: add round info to page and allow going to previous round

--- src/components/rule_display.rs
+++ src/components/rule_display.rs
@@ -6,9 +6,13 @@ pub fn RuleDisplay(#[prop(into)] rule: Signal<usize>) -> impl IntoView {
     let rule = Signal::derive(move || RULES.get().unwrap().get(rule.get()).unwrap().clone());
 
     view! {
-        <div class="w-full max-w-md p-6">
-            {move || rule.get().name} <br/>
+        <div class="flex flex-col w-full h-1/2 max-w-md text-center text-lg gap-y-10">
+            <div class="w-full">
+            {move || rule.get().name}
+            </div>
+            <div class="w-full">
             {move || rule.get().description}
+            </div>
         </div>
     }
 }
--- src/pages/seed_display.rs
+++ src/pages/seed_display.rs
@@ -50,15 +50,22 @@ pub fn SeedDisplay() -> impl IntoView {
     });
 
     view! {
-        <div class="min-h-screen bg-gray-100 flex flex-col items-center">
+        <div class="min-h-screen h-screen bg-gray-100 flex flex-col items-center">
+            <h1 class="text-2xl font-bold text-gray-800 text-center mb-4">{move || format!("Round {}", round())}</h1>
             <RuleDisplay rule/>
-            <div class="w-full max-w-md p-6">
-            <A href=move || format!("/beerio/?seed={}&player={}", seed(), player()) >
-                <Button>BACK</Button>
-            </A>
-            <A href=move ||  format!("?player={}&round={}", player(), round()+1)>
-                <Button>NEXT</Button>
-            </A>
+            <div class="absolute bottom-20 flex flex-row w-full max-w-md gap-x-4 items-end justify-center">
+                <Show when=move || 1 < round() >
+                    <div class="w-1/2">
+                    <A href=move || format!("?player={}&round={}", player(), round()-1) >
+                        <Button>PAST ROUND</Button>
+                    </A>
+                    </div>
+                </Show>
+                <div class="w-1/2">
+                <A href=move ||  format!("?player={}&round={}", player(), round()+1)>
+                    <Button>NEXT ROUND </Button>
+                </A>
+                </div>
             </div>
         </div>
 
